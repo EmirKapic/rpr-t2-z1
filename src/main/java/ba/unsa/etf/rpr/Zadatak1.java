29a3627b88231dff03ae6761249afb1d43bc3355
EmirKapic <ekapic1@etf.unsa.ba>
2022-10-24 17:50:33 +0200

    1

--- src/main/java/ba/unsa/etf/rpr/Zadatak1.java
+++ src/main/java/ba/unsa/etf/rpr/Zadatak1.java
@@ -1,7 +1,7 @@
 package ba.unsa.etf.rpr;
 
-public class Main {
+public class Zadatak1 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("5");
     }
 }
