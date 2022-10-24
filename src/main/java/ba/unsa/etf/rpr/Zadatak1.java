140d042b08849794509de5fe124c8af2533ce193
EmirKapic <ekapic1@etf.unsa.ba>
2022-10-24 18:32:03 +0200

    1

--- src/main/java/ba/unsa/etf/rpr/Zadatak1.java
+++ src/main/java/ba/unsa/etf/rpr/Zadatak1.java
@@ -1,7 +1,15 @@
 package ba.unsa.etf.rpr;
 
+import static ba.unsa.etf.rpr.Faktorijel.fact;
+
 public class Zadatak1 {
     public static void main(String[] args) {
-        System.out.println("5");
+        if (args[0].equals("fact")){
+            Integer x = fact(Integer.valueOf(args[1]));
+            System.out.println(x);
+        }
+        if (args[0].equals("sin")){
+            System.out.println(Math.sin(Double.parseDouble(args[1])));
+        }
     }
 }
